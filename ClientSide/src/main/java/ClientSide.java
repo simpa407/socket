54c2f9a9fd9dcfeb2ab7b3ad2d833238dd3a39c1
Simpa407 <simpa407@gmail.com>
2020-03-15 22:12:28 +0700

    complete

--- ClientSide/src/main/java/ClientSide.java
+++ ClientSide/src/main/java/ClientSide.java
@@ -1,43 +1,66 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
-
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+ 
 /**
- *
- * @author giasutinhoc.vn
+ *class thực hiện các chức năng bên phía client
  */
 public class ClientSide {
-
+ 
+    public final static String SERVER_IP = "127.0.0.1"; //địa chỉ IP của server
+    public final static int SERVER_PORT = 5000; // cổng server
+    public final static byte[] BUFFER = new byte[4096]; // Vùng đệm chứa dữ liệu cho gói tin nhận
+ 
     public static void main(String[] args) {
+        
+        //tạo socket kiểu UDP
+        DatagramSocket dataSocket = null;
         try {
-            //Tạo đối tượng Socket đại diện cho cs
-            Socket cs = new Socket("localhost", 5020);
-
-            //Tạo đối tượng dùng để nhận dữ liệu từ server
-            InputStream is = cs.getInputStream();
-            Scanner s = new Scanner(is);
-            //Tạo đối tượng dùng để gửi dữ liệu đến server
-            OutputStream os = cs.getOutputStream();
-            PrintWriter ps = new PrintWriter(os, true);
-            //Gửi dữ liệu đến server
+            dataSocket = new DatagramSocket();
+            System.out.println("Client started ");
+ 
+            InetAddress server = InetAddress.getByName(SERVER_IP);
             while (true) {
-                InputStreamReader isr = new InputStreamReader(System.in); // Nhập
-                BufferedReader br = new BufferedReader(isr); // một chuỗi
-                String theString = br.readLine();
-                ps.println(theString);
-                System.out.println("CLient send to server " + theString);
-                //Nhận dữ liệu từ server
-                String strReceive = s.nextLine();
-                System.out.println("Receive from server: " + strReceive);
+                
+                //Nhập thông tin mã sinh viên cần tìm kiếm từ bàn phím
+                System.out.println("Nhap MSSV: ");
+                InputStreamReader input = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(input);
+                //tạo biến lưu mã số vừa nhập
+                String mssv = br.readLine();
+                
+                //chuyển mã số sang kiểu byte
+                byte[] data = mssv.getBytes();
+ 
+                // Tạo gói tin gửi đến server
+                DatagramPacket packet = new DatagramPacket(data, data.length, server, SERVER_PORT);
+                //gửi gói tin sang server
+                dataSocket.send(packet);
+                
+                //nhập Over để đóng kết nối với server
+                if(mssv.equals("over")){
+                    dataSocket.close();
+                }
+ 
+                // tạo gói tin nhận
+                DatagramPacket packetReceive = new DatagramPacket(BUFFER, BUFFER.length);
+                //nhận gói tin gửi về từ server
+                dataSocket.receive(packetReceive);
+ 
+                // hiển thị thông tin nhận được
+                System.out.println("Thong tin sinh vien: " + new String(packetReceive.getData(), 0, packetReceive.getLength()));
             }
-        } catch (IOException ex) {
+        } catch (IOException e) {
+            //hiển thị lỗi
+            System.err.println(e);
+        } finally {
+            //đóng kết nối
+            if (dataSocket != null) {
+                dataSocket.close();
+            }
         }
-
     }
 }
--- ServerSide/src/main/java/ServerSide.java
+++ ServerSide/src/main/java/ServerSide.java
@@ -1,58 +1,86 @@
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+/**
+ *
+ * class thực hiện các chức năng bên server
+ */
 public class ServerSide {
 
-    public static void main(String[] args) throws IOException {
+    public final static int SERVER_PORT = 5000; // cổng server
+    public final static byte[] BUFFER = new byte[4096];
+
+    /**
+     * hàm main thực hiện các chức năng của server
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        //tạo danh sách sinh viên
         ArrayList<Student> students = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Student person = new Student("mssv" + i, "name" + i, "email" + i, "class" + i, "date" + i, "country" + i);
+            Student person = new Student("mssv" + i, "name" + i, "email" + i, "class" + i, "date" + i, "country" + i, "phone" + i);
             students.add(person);
         }
+        
+        //tạo socket kiểu UDP
+        DatagramSocket dataSocket = null;
         try {
-            System.out.println("Binding to port 5001, please wait  ...");
-            ServerSocket serverSocket = new ServerSocket(5020);
-            System.out.println("Server started: " + serverSocket);
-            System.out.println("Waiting for a client ...");
+            System.out.println("Binding... " + SERVER_PORT);
+            dataSocket = new DatagramSocket(SERVER_PORT);
+            System.out.println("Server started ");
+            System.out.println("Waiting for messages from Client ... ");
+
             while (true) {
-                try {
-                    try (Socket socket = serverSocket.accept()) {
-                        System.out.println("Client accepted: " + socket);
+                //tạo gói tin nhận
+                DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
+                dataSocket.receive(packet); // Chờ nhận gói tin
 
-                        InputStream is = socket.getInputStream();
-                        Scanner s = new Scanner(is);
-
-                        OutputStream os = socket.getOutputStream();
-                        PrintWriter pw = new PrintWriter(os, true);
-
-                        while (true) {
-                            String strReceive = s.nextLine();
-                            System.out.println("Receive from client: " + strReceive);
-                            while (!strReceive.equals("Over")) {
-                                for (int i = 0; i < students.size(); i++) {
-                                    if (students.get(i).id == null ? strReceive == null : students.get(i).id.equals(strReceive)) {
-                                        pw.println(students.get(i).id + students.get(i).name);
-                                        break;
-                                    }
-                                }
-                            }
-                            System.out.println("Closing connection");
-                            serverSocket.close();
-                            is.close();
-                        }
-                    }
-                } catch (IOException e) {
-                    System.err.println(" Connection Error: " + e);
+                // Lấy dữ liệu khỏi gói tin nhận
+                String message = new String(packet.getData(), 0, packet.getLength());
+                
+                //Hiển thị thông tin nhận được từ client
+                System.out.println("Received from client: " + message);
+                
+                //đóng kết nối khi nhận được thông tin từ client
+                if(message.equals("over")){
+                    dataSocket.close();
                 }
+                
+                Student data = null; //khai báo sinh viên cần lấy dữ liệu
+                
+                for (int i = 0; i < students.size(); i++) {
+                    if (students.get(i).id.equals(message)) {
+                        data = new Student(students.get(i)); // lưu dữ liệu sinh viên tìm được
+                        break;
+                    }
+                }
+                
+                //tạo nội dung gói tin gửi về client
+                if(data == null){
+                    //khong tim thay sinh vien
+                    message = "Khong ton tai";
+                } else {
+                    //thong tin sinh vien tim duoc
+                     message = "ho ten: " + data.name + " MSSV: " + data.id + " lop: " + data._class + " so dien thoai: " + data.phone + " email: " + data.email + " dia chi: " + data.country;
+                }
+                
+                // Tạo gói tin gởi chứa dữ liệu vừa nhận được
+                DatagramPacket dataPacket = new DatagramPacket(message.getBytes(), message.length(),
+                        packet.getAddress(), packet.getPort());
+                //Gửi về client
+                dataSocket.send(dataPacket);
             }
         } catch (IOException e) {
+            //in ra lỗi
+            System.out.println(e);
+        } finally {
+            //đóng kết nối
+            if (dataSocket != null) {
+                dataSocket.close();
+            }
         }
     }
 }
